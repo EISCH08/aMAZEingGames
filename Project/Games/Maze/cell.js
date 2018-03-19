4eb57e17b34c9c7f71980ee77a2b9f193eb6b04c
JustinVelvick <36527429+JustinVelvick@users.noreply.github.com>
2018-03-18 22:52:06 -0600

    Add files via upload
    Added the player file, added ability for player to walk through the maze to a randomly generated exit. Right now the player just teleports back to spawn with the same maze, but I will try to get the program to generate a new maze instead. Basically I need to learn how to restart the program, probably through html.

--- Project/Games/Maze/cell.js
+++ Project/Games/Maze/cell.js
@@ -9,7 +9,23 @@ function Cell(i, j){ //i is the column, j is the row
    this.y = j;
    this.walls = [true, true, true, true]; //array of walls(true is has wall) top right bottom left
    this.visited = false;
+   this.exit = false;
 
+   this.exitChance = function(){
+      if(exit == 1){ //we already have an exit
+         return false;
+      }
+      if(j == (height/size1) -1 && i > width/(2*size1) ||
+         j > height/(2*size1) && i == (width/size1) -1) //if this cell is on the bottom right size of maze)
+      {
+         var ran = floor(random(0,2));
+         if(ran == 1)
+         {
+            exit = 1; //we now have our exit
+            this.exit = true; //this cell is the exit
+         }
+      }
+   };
 
    this.checkNeighbors = function() {
       var neighbors = []; //unvisted neighbors
@@ -58,7 +74,7 @@ function Cell(i, j){ //i is the column, j is the row
 
 
    this.show = function() {
-      var x = this.x*size1; //ie co1l 2 will net an x position of 2 times the size of each square (putting it in the second square on the grid)
+      var x = this.x*size1; //ie co1 2 will net an x position of 2 times the size of each square (putting it in the second square on the grid)
       var y = this.y*size1; //same here
       stroke(255);
 
@@ -66,10 +82,15 @@ function Cell(i, j){ //i is the column, j is the row
          line(x,y, x+size1, y);//top
       }
       if(this.walls[1]){
-         line(x+size1,y,x+size1, y+size1);//right
+         line(x+size1, y, x+size1, y+size1);//right
       }
-      if(this.walls[2]){
+      if(this.walls[2] || this.walls[2] == -1){
+         if(this.walls[2] == -1)
+         {
+            stroke(255,0,0);
+         }
          line(x+size1,y+size1, x, y+size1);//bottom
+         stroke(255);
       }
       if(this.walls[3]){
          line(x,y+size1, x, y);//left
@@ -77,8 +98,15 @@ function Cell(i, j){ //i is the column, j is the row
 
       if(this.visited){
          noStroke();
-         fill(255,0,255, 100);
-         rect(x, y, size1, size1);
+         if(this.exit)
+         {
+            fill(255, 225, 0);
+            rect(x, y, size1, size1);
+         }
+         else{
+            fill(255,0,255, 100);
+            rect(x, y, size1, size1);
+         }
       }
    };
 
--- Project/Games/Maze/maze.js
+++ Project/Games/Maze/maze.js
@@ -1,20 +1,23 @@
 var cols,rows;
 var size1 = 40;
 var grid = [];
-
+var user; //0 is generating the maze, once done player = 1
+//keeps player locked out from controls until maze is done generating
 var current;
-
 var stack = [];
+var exit = 0; //will changed to 1 once an exit is made
 
 function setup(){
-   createCanvas(400,400);
+   createCanvas(600,600);
    cols = floor(width/size1); //x cols exist
    rows = floor(height/size1);//x rows exist
-   frameRate(5);
+   frameRate(40);
+   user = new Player();
 
    for (var j = 0; j < rows; j++){
       for(var i = 0; i < cols; i++){
          var newcell = new Cell(i,j); //ie send col 2 row 5
+         newcell.exitChance();
          grid.push(newcell);
       }
    }
@@ -24,60 +27,99 @@ function setup(){
 
 function draw(){
    background(51);
-
-   current.visited = true; //current cell has been visited
-   current.highlight(); //visually show which cell is current
-   for (var i = 0; i < grid.length; i++){
-      grid[i].show();
+   if(user.players == 1){ ////switch from maze generation code to player code
+      frameRate(10);
+      user.show();
    }
 
-   var nextCell = current.checkNeighbors();
+   else{ //use this code if maze is generating
+      current.visited = true; //current cell has been visited
+      current.highlight(); //visually show which cell is current
+      for (var i = 0; i < grid.length; i++){
+         grid[i].show();
+      }
 
-   if(nextCell){ //if there is a neighbor to go to at all
-      removeWalls(current, nextCell); //get rid of lines to make maze path before
-                                      //show is called
-      nextCell.visited = true;
-      stack.push(current); //add current cell for future backtracking
+      var nextCell = current.checkNeighbors(); //find next neighbor to visit
 
-      current = nextCell;
+      //once the maze is complete, stop looping and let the player play
+      //no new cells to pop, located at top left, new neighbor doesnt exist
+      if(stack.length == 0 && current.x == 0 && current.y == 0 && !nextCell){
+         user.players = 1;
+      }
+
+      if(nextCell){ //if there is a neighbor to go to
+         removeWalls(current, nextCell); //get rid of lines to make maze path before show is called
+         nextCell.visited = true;
+         stack.push(current); //add current cell for future backtracking
+
+         current = nextCell;
+      }
+      else if(stack.length > 0){ //no available neighbors, start popping off the stack to go back
+         var previousCell = stack.pop();
+         current = previousCell;
+      }
    }
-   else if(stack.length > 0){ //no available neighbors, start popping off the stack to go back
-      var previousCell = stack.pop();
-      current = previousCell;
+      function removeWalls(a, b){
+      var x = a.x - b.x; //difference in x location of the current and next cell
+      if(x == 1){
+         a.walls[3] = false; //left wall removed from current
+         b.walls[1] = false; //right wall removed from next
+      }
+      else if(x == -1)
+      {
+         a.walls[1] = false;
+         b.walls[3] = false;
+      }
+
+      var y = a.y - b.y; //difference in y location of the current and next cell
+      if(y == 1){
+         a.walls[0] = false; //top wall removed from current
+         b.walls[2] = false; //bottom wall removed from next
+      }
+      else if(y == -1)
+      {
+         a.walls[2] = false;
+         b.walls[0] = false;
+      }
    }
 }
 
-function removeWalls(a, b){
-   var x = a.x - b.x; //difference in x location of the current and next cell
-   if(x == 1){
-      a.walls[3] = false; //left wall removed from current
-      b.walls[1] = false; //right wall removed from next
-   }
-   else if(x == -1)
-   {
-      a.walls[1] = false;
-      b.walls[3] = false;
-   }
-
-   var y = a.y - b.y; //difference in y location of the current and next cell
-   if(y == 1){
-      a.walls[0] = false; //top wall removed from current
-      b.walls[2] = false; //bottom wall removed from next
-   }
-   else if(y == -1)
-   {
-      a.walls[2] = false;
-      b.walls[0] = false;
-   }
+function keyPressed() {
+   console.log(current);
+  if (keyCode === UP_ARROW && user.players == 1) {
+     if(current.walls[0]){
+        current = current;
+     }
+      else{
+         current = grid[index(current.x, current.y-1)];
+      }
+  }
+  if (keyCode === DOWN_ARROW && user.players == 1) {
+     if(current.walls[2]){
+        current = current;
+     }
+      else {
+         current = grid[index(current.x, current.y+1)];
+      }
+  }
+  if (keyCode === RIGHT_ARROW && user.players == 1) {
+     if(current.walls[1]){
+        current = current;
+     }
+      else{
+         current = grid[index(current.x+1, current.y)];
+      }
+  }
+  if (keyCode === LEFT_ARROW && user.players == 1) {
+     if(current.walls[3]){
+        current = current;
+     }
+      else{
+         current = grid[index(current.x-1, current.y)];
+      }
+  }
+  if(current.exit){
+     console.log('you win');
+     current = grid[0];
+ }
 }
-
-
-
-
-
-
-
-
-
-
-//.
