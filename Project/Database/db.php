3859c9d6d345f09ae8c555d7bf191f5a9a062ea6
Vlad Zhdanov <vzhdanov99@gmail.com>
2018-05-03 15:36:47 -0600

    cleaning up files

--- Project/Database/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'pass123';
-$db = 'Accounts';
-$mysqli = new mysqli($host,$user, $pass, $db) or die($mysqli->error);
